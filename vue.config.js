a7528559994465a28abbee2ca6ddc8c1cbd761ed
AzuraXW <2219444825@qq.com>
2021-03-19 12:46:19 +0800

    打包优化2

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,27 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       config.plugin('html')
-      .tap(args => {
-        args[0].isProd = true
-        return args
-      })
-
+        .tap(args => {
+          args[0].isProd = true
+          return args
+        })
     }
   },
   productionSourceMap: false,
-  configureWebpack: {
-    externals: process.env.NODE_ENV === 'production' && {
-      'vue': 'Vue',
-      'vue-router': 'VueRouter',
-      'vuex': 'Vuex',
-      'element-ui': 'elementUI',
-      'axios': 'axios',
-      'marked': 'marked',
-      'highlight': 'hljs'
+  configureWebpack: config => {
+    // 利用环境变量进行判断是否是生产环境production
+    if (process.env.NODE_ENV === 'production') {
+      config.externals = {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex',
+        'element-ui': 'elementUI',
+        axios: 'axios',
+        marked: 'marked',
+        highlight: 'hljs'
+      }
     }
   }
 }
